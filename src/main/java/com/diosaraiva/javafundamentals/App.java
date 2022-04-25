cee76fa833a6d96eff6714c1adaec8b7e95bf613
Diogo Saraiva <diosaraiva@hotmail.com>
2022-04-25 00:43:41 -0300

    Console updates.

--- src/main/java/com/diosaraiva/javafundamentals/App.java
+++ src/main/java/com/diosaraiva/javafundamentals/App.java
@@ -1,51 +1,7 @@
 package com.diosaraiva.javafundamentals;
 
-import com.diosaraiva.javafundamentals.designpatterns.creational.abstractfactory.AbstractFactory;
-import com.diosaraiva.javafundamentals.designpatterns.creational.builder.Builder;
-import com.diosaraiva.javafundamentals.designpatterns.creational.factorymethod.FactoryMethod;
-import com.diosaraiva.javafundamentals.designpatterns.creational.objectpool.ObjectPoolPattern;
-import com.diosaraiva.javafundamentals.designpatterns.creational.prototype.PrototypePattern;
-import com.diosaraiva.javafundamentals.designpatterns.structural.adapter.Adapter;
-import com.diosaraiva.javafundamentals.designpatterns.structural.bridge.Bridge;
-import com.diosaraiva.javafundamentals.designpatterns.structural.composite.Composite;
-import com.diosaraiva.javafundamentals.designpatterns.structural.decorator.Decorator;
-import com.diosaraiva.javafundamentals.designpatterns.structural.facade.Facade;
-import com.diosaraiva.javafundamentals.designpatterns.structural.proxy.Proxy;
-import com.diosaraiva.javafundamentals.interfaces.collection.Lists;
-import com.diosaraiva.javafundamentals.properties.PropertiesUtils;
-
 public class App{
 	public static void main(String[] args){
-		boolean loop = true;
-
-		try {
-			while(loop){
-				switch (AppOptions.getUserOption()){
-				case 0: loop = false; 						AppOptions.exitProgram(); break;
-
-				case 1: FactoryMethod.GenerateBill();		AppOptions.pressToContinue(); break;
-				case 2: Builder.BuildCds(); 				AppOptions.pressToContinue(); break;
-				case 3: AbstractFactory.GetLoan();			AppOptions.pressToContinue(); break;
-				case 4: PrototypePattern.PrintRecords();	AppOptions.pressToContinue(); break;
-				case 5: ObjectPoolPattern.TestObjectPool();	AppOptions.pressToContinue(); break;
-				case 6: Adapter.GetCreditCard();			AppOptions.pressToContinue(); break;
-				case 7: Lists.printArrayList();				AppOptions.pressToContinue(); break;
-				case 8: Lists.printLinkedList();			AppOptions.pressToContinue(); break;
-				case 9: Lists.printVector();				AppOptions.pressToContinue(); break;
-				case 10: Lists.printStack();				AppOptions.pressToContinue(); break;
-				case 11: Bridge.askQuestions();				AppOptions.pressToContinue(); break;
-				case 12: Composite.getCashiers();			AppOptions.pressToContinue(); break;
-				case 13: Decorator.orderFood();				AppOptions.pressToContinue(); break;
-				case 14: Facade.getMobileShop();			AppOptions.pressToContinue(); break;
-				case 15: Proxy.grantAccess();				AppOptions.pressToContinue(); break;
-				case 16: PropertiesUtils.ReadProperties();	AppOptions.pressToContinue(); break;
-
-				default: AppOptions.getInvalidOption(); break;
-				}
-			}
-		} catch (Exception e){
-			AppOptions.getError(e);
-			AppOptions.exitProgram();
-		}
+		while(AppOptions.runConsole());
 	}
 }
--- src/main/java/com/diosaraiva/javafundamentals/AppOptions.java
+++ src/main/java/com/diosaraiva/javafundamentals/AppOptions.java
@@ -2,37 +2,28 @@ package com.diosaraiva.javafundamentals;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
 
+import com.diosaraiva.javafundamentals.designpatterns.creational.abstractfactory.AbstractFactory;
+import com.diosaraiva.javafundamentals.designpatterns.creational.builder.Builder;
+import com.diosaraiva.javafundamentals.designpatterns.creational.factorymethod.FactoryMethod;
+import com.diosaraiva.javafundamentals.designpatterns.creational.objectpool.ObjectPoolPattern;
+import com.diosaraiva.javafundamentals.designpatterns.creational.prototype.PrototypePattern;
+import com.diosaraiva.javafundamentals.designpatterns.structural.adapter.Adapter;
+import com.diosaraiva.javafundamentals.designpatterns.structural.bridge.Bridge;
+import com.diosaraiva.javafundamentals.designpatterns.structural.composite.Composite;
+import com.diosaraiva.javafundamentals.designpatterns.structural.decorator.Decorator;
+import com.diosaraiva.javafundamentals.designpatterns.structural.facade.Facade;
+import com.diosaraiva.javafundamentals.designpatterns.structural.proxy.Proxy;
+import com.diosaraiva.javafundamentals.interfaces.collection.Lists;
+import com.diosaraiva.javafundamentals.properties.PropertiesUtils;
+
 public class AppOptions{
-	public static Map<Integer,String> getOptionList(){
-		Map<Integer, String> optionList = new HashMap<Integer, String>();
-		optionList.put(1, "design-patterns/creational/factorymethod");
-		optionList.put(2, "design-patterns/creational/builder");
-		optionList.put(3, "design-patterns/creational/abstractfactory");
-		optionList.put(4, "design-patterns/creational/prototype");
-		optionList.put(5, "design-patterns/creational/objectpool");
-		optionList.put(6, "design-patterns/structural/adapter");
-		optionList.put(7, "interfaces/collection/arraylist");
-		optionList.put(8, "interfaces/collection/linkedlist");
-		optionList.put(9, "interfaces/collection/vector");
-		optionList.put(10, "interfaces/collection/stack");
-		optionList.put(11, "design-patterns/structural/bridge");
-		optionList.put(12, "design-patterns/structural/composite");
-		optionList.put(13, "design-patterns/structural/decorator");
-		optionList.put(14, "design-patterns/structural/facade");
-		optionList.put(15, "design-patterns/structural/proxy");
-		optionList.put(16, "properties/read");
-
-		return optionList;
-	}
-
-	public static int getUserOption(){
+	public static boolean runConsole(){
 		Set<Entry<Integer, String>> optionList = getOptionList().entrySet();
 		System.out.println("----------------[ AVAILABLE OUTPUTS: " + optionList.size() + " OPTIONS ]----------------");
 		optionList.stream().forEach(item -> System.out.println(item.getKey() + ". " + item.getValue()));
@@ -41,36 +32,88 @@ public class AppOptions{
 
 		System.out.print("\nEnter output option number: ");
 
-		int option;
+		try{
+			switch (readUserOption()){
+			case 0:	exitProgram(); return false;
+
+			case 1: FactoryMethod.GenerateBill();		pressToContinue(); return true;
+			case 2: Builder.BuildCds(); 				pressToContinue(); return true;
+			case 3: AbstractFactory.GetLoan();			pressToContinue(); return true;
+			case 4: PrototypePattern.PrintRecords();	pressToContinue(); return true;
+			case 5: ObjectPoolPattern.TestObjectPool();	pressToContinue(); return true;
+			case 6: Adapter.GetCreditCard();			pressToContinue(); return true;
+			case 7: Lists.printArrayList();				pressToContinue(); return true;
+			case 8: Lists.printLinkedList();			pressToContinue(); return true;
+			case 9: Lists.printVector();				pressToContinue(); return true;
+			case 10: Lists.printStack();				pressToContinue(); return true;
+			case 11: Bridge.askQuestions();				pressToContinue(); return true;
+			case 12: Composite.getCashiers();			pressToContinue(); return true;
+			case 13: Decorator.orderFood();				pressToContinue(); return true;
+			case 14: Facade.getMobileShop();			pressToContinue(); return true;
+			case 15: Proxy.grantAccess();				pressToContinue(); return true;
+			case 16: PropertiesUtils.ReadProperties();	pressToContinue(); return true;
+
+			default: getInvalidOption(); return true;
+			}
+		} catch (Exception e){
+			getError(e);
+			return true;
+		}
+	}
+
+	private static Map<Integer,String> getOptionList(){
+		Map<Integer, String> optionList = new HashMap<Integer, String>();
+		int i = 0;
+		optionList.put(++i, "design-patterns/creational/factorymethod");
+		optionList.put(++i, "design-patterns/creational/builder");
+		optionList.put(++i, "design-patterns/creational/abstractfactory");
+		optionList.put(++i, "design-patterns/creational/prototype");
+		optionList.put(++i, "design-patterns/creational/objectpool");
+		optionList.put(++i, "design-patterns/structural/adapter");
+		optionList.put(++i, "interfaces/collection/arraylist");
+		optionList.put(++i, "interfaces/collection/linkedlist");
+		optionList.put(++i, "interfaces/collection/vector");
+		optionList.put(++i, "interfaces/collection/stack");
+		optionList.put(++i, "design-patterns/structural/bridge");
+		optionList.put(++i, "design-patterns/structural/composite");
+		optionList.put(++i, "design-patterns/structural/decorator");
+		optionList.put(++i, "design-patterns/structural/facade");
+		optionList.put(++i, "design-patterns/structural/proxy");
+		optionList.put(++i, "properties/read");
+
+		return optionList;
+	}
+
+	private static int readUserOption(){
 		try{
 			//Read user input
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			option=Integer.parseInt(br.readLine());
+			int option = Integer.parseInt(br.readLine());
+			
+			if(option!=0) System.out.println("\n----------------[ OUTPUT: <" + option + ". " + getOptionList().get(option) + "> ]----------------");
+			
+			return option;
 		} catch (Exception e){
 			//if error, invalid option!
-			option = 999;
+			return 999;
 		}
-
-		if(option!=0) System.out.println("\n----------------[ OUTPUT: <" + option + ". " + getOptionList().get(option) + "> ]----------------");
-
-		return option;
 	}
 
-	public static void pressToContinue(){
+	private static void pressToContinue(){
 		System.out.println("\nPress ENTER to continue...");
 		Scanner s = new Scanner(System.in);
 		s.nextLine();
 	}
 
-	public static void getInvalidOption(){
+	private static void getInvalidOption(){
 		System.out.println("INVALID OPTION\n");
 	}
 
-	public static void getError(Exception e){
-		System.out.println("\n[ ERROR: " + e.toString() + " ]");
+	private static void getError(Exception e){
+		System.out.println("\n[ ERROR: " + e.toString() + " ]\n");
 	}
 
-	public static void exitProgram(){
+	private static void exitProgram(){
 		System.out.println("\n----------------[ EXITING PROGRAM ]----------------");
 	}
 }
